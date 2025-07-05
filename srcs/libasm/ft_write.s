c8b9087ba27b6a340def0f37974a813a79fccd99
LEE LUNA <seojilee@student.42seoul.kr>
2025-07-05 16:09:50 +0900

    Comment: correct grammar

--- srcs/libasm/ft_write.s
+++ srcs/libasm/ft_write.s
@@ -9,7 +9,7 @@ ft_write:
 	syscall
 	cmp rax, 0
 	jge .done ; rax >= 0
-	push rax ; rax: caller-saved. might be overwritten after func call.
+	push rax ; rax: caller-saved. might be overwritten after a func call.
 	call __errno_location wrt ..plt ; PIE-supporting
 	pop rcx ; rcx: caller-saved. can be used as a temp storage.
 	neg rcx
