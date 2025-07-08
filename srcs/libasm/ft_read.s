c064aa121459bab4d716e011dc7d1dd252b49298
LEE LUNA <seojilee@student.42seoul.kr>
2025-07-08 16:41:53 +0900

    add read

---
+++ srcs/libasm/ft_read.s
@@ -0,0 +1,24 @@
+; syscall number 0: /usr/include/bits/syscall.h
+
+; ssize_t read(int filedes, void* buf, size_t nbytes);
+; RETURN VALUES:
+; 	if successful, the number of bytes actually read is returned.
+; 	else, -1 is returned. (and errno is set.)
+
+extern __errno_location ; external function from standard c library
+
+section .text
+global ft_read
+ft_read:
+	mov rax, 0
+	syscall
+	cmp rax, 0
+	jge .done
+	push rax
+	call __errno_location wrt ..plt
+	pop rcx
+	neg rcx
+	mov [rax], rcx
+	mov rax, -1
+.done:
+	ret
