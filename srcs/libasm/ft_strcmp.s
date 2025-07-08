0ed8e6df9967a1d8ba0f57e344154b1c8d165c22
LEE LUNA <seojilee@student.42seoul.kr>
2025-07-08 16:48:27 +0900

    minimize line

--- srcs/libasm/ft_strcmp.s
+++ srcs/libasm/ft_strcmp.s
@@ -1,9 +1,6 @@
 ; int ft_strcmp(const char* s1, const char* s2) {
 ;	size_t i = 0;
-; 	while (s1[i] && s2[i]) {
-; 		if (s1[i] != s2[i]) {
-; 			break;
-; 		}
+; 	while (s1[i] && s2[i] && s1[i] == s2[i]) {
 ; 		++i;
 ; 	}
 ; 	return s1[i] - s2[i];
