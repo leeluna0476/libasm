f3ed30e5caae16f72f5674c255a047145f51534b
LEE LUNA <seojilee@student.42seoul.kr>
2025-07-08 16:39:40 +0900

    remove unnecessary comments

--- srcs/libasm/ft_write.s
+++ srcs/libasm/ft_write.s
@@ -1,19 +1,24 @@
-; ssize_t write(int filedes, const void* buf, size_t nbyte); // syscall number = 1
+; syscall number 1: /usr/include/bits/syscall.h
+
+; ssize_t write(int filedes, void* buf, size_t nbytes);
+; RETURN VALUES:
+; 	if successful, the number of bytes actually written is returned.
+; 	else, -1 is returned. (and errno is set.)
 
 extern __errno_location ; external function from standard c library
 
 section .text
 global ft_write
 ft_write:
-	mov rax, 1 ; set the syscall number
+	mov rax, 1
 	syscall
 	cmp rax, 0
-	jge .done ; rax >= 0
-	push rax ; rax: caller-saved. might be overwritten after func call.
-	call __errno_location wrt ..plt ; PIE-supporting
-	pop rcx ; rcx: caller-saved. can be used as a temp storage.
+	jge .done
+	push rax
+	call __errno_location wrt ..plt
+	pop rcx
 	neg rcx
-	mov [rax], rcx ; set errno.
+	mov [rax], rcx
 	mov rax, -1
 .done:
 	ret
