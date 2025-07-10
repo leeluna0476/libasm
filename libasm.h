7e5f1118a0297c7eeb1a6823514b69f8e75881fb
LEE LUNA <leeluna0476@daum.net>
2025-07-10 15:14:56 +0900

    add const keyword to the parameter buf of write

--- libasm.h
+++ libasm.h
@@ -8,6 +8,6 @@ int   	ft_strcmp(const char* s1, const char* s2);
 char*   ft_strcpy(char* dst, const char* src);
 char*   ft_strdup(const char* s);
 ssize_t ft_read(int filedes, void* buf, size_t nbytes);
-ssize_t ft_write(int filedes, void* buf, size_t nbytes);
+ssize_t ft_write(int filedes, const void* buf, size_t nbytes);
 
 #endif
